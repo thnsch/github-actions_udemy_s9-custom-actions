eb97b6e099246b95b90364890c4f3e2c0b3032e0
TS <thn.schmitt@gmail.com>
2024-10-04 13:39:22 +0200

    added deploy custom action - fails here

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,18 @@ const github = require('@actions/github')
 const exec = require('@actions/exec')
 
 function run() {
+  // 1) get some input values
+  const bucket = core.getInput('bucket', { required: true })
+  const bucketRegion = core.getInput('bucket-region', { required: true })
+  const distFolder = core.getInput('dist-folder', { required: true })
+  
+  // 2) upload files
+  const s3Uri = `s3://${bucket}`                                            // back-ticks instead of quotes
+  exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`)   // AWS CLI is preinstalled on the Runner on ubuntu-latest 
+
+  // github.getOctokit()...  // 
+  // github.context...       // get context info e.g. action
+
   core.notice('Hello from my custom JavaScript Action!');
 } 
 
